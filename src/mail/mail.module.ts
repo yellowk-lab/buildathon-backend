3a4297a6c922b0b876054b3a1e63c75e98b2a6cf
Samy Abouseda <38294863+jarvistatch@users.noreply.github.com>
2024-06-14 10:36:21 +0200

    Remove outdated and broken node mailer library.

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,38 +1,9 @@
-import { MailerModule } from '@nestjs-modules/mailer';
 import { Global, Module } from '@nestjs/common';
 import { MailService } from './mail.service';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { join } from 'path';
-import { ConfigService } from '@nestjs/config';
 
 @Global()
 @Module({
-  imports: [
-    MailerModule.forRootAsync({
-      useFactory: async (config: ConfigService) => ({
-        transport: {
-          host: config.get('MAIL_HOST'),
-          port: config.get('MAIL_PORT'),
-          auth: {
-            user: config.get('MAIL_USER'),
-            pass: config.get('MAIL_PASSWORD'),
-          },
-        },
-        defaults: {
-          from: config.get('MAIL_FROM'),
-        },
-        preview: true,
-        template: {
-          dir: join(__dirname, '/../../src/mail/templates'),
-          adapter: new HandlebarsAdapter(),
-          options: {
-            strict: true,
-          },
-        },
-      }),
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [],
   providers: [MailService],
   exports: [MailService],
 })
